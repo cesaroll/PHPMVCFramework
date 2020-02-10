8f24549ce69db2c984d1952ed1c47aff6f80682c
Cesar Lopez <clopezlerma@wayfair.com>
2020-02-10 21:56:37 +0100

    Match thee requested route to the list of routes in the routing table

--- Core/Router.php
+++ Core/Router.php
@@ -15,6 +15,12 @@ class Router
   protected $routes = [];
 
   /**
+   * PArameters from the matched route
+   * @var array
+   */
+  protected $params = [];
+
+  /**
    * Add a route to the routing table
    *
    * @param string $route The route URL
@@ -31,6 +37,36 @@ class Router
    */
   public function getRoutes(){
     return $this->routes;
+  }
+
+  /**
+   * Match the route to the routes in the routing table, setting the $params
+   * property if a route is found.
+   *
+   * @param string $url The route url
+   *
+   * @return bool true if a match is found, false otherwise
+   */
+  public function match($url): bool {
+
+    foreach($this->routes as $route => $params){
+      if($url == $route){
+        $this->params = $params;
+        return true;
+      }
+    }
+
+    return false;
+
+  }
+
+  /**
+   * Get the currently matched parameters
+   *
+   * @return array
+   */
+  public function getParams(){
+    return $this->params;
   }
 
 }
