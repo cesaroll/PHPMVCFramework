a7ec3486ff0f3a937b43ab693ba277afa1e6c0b9
Cesar Lopez <clopezlerma@wayfair.com>
2020-02-23 19:04:05 +0100

    REplace autoload function with the composer autoloader

--- public/index.php
+++ public/index.php
@@ -3,23 +3,7 @@
 /**
  * Twig
  */
-require_once dirname(__DIR__) . '/vendor/autoload.php';
-
-/**
- * Autoloader
- */
-spl_autoload_register(function ($class){
-
-  $root = dirname(__DIR__); //Get the parent folder
-  $file = $root . '/' . str_replace('\\','/',$class) . '.php';
-
-  if(is_readable($file)) {
-    require $root . '/' . str_replace('\\','/',$class) . '.php';
-  }
-
-});
-
-
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 $router = new Core\Router();
 
